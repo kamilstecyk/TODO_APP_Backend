c433f79752914dd01c16743d82e51b300714cc94
Kamil Stecyk <kamils987@interia.eu>
2023-04-02 14:15:14 +0200

    ready template json-server

---
+++ server.js
@@ -0,0 +1,18 @@
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+	// Add custom route here if needed
+);
+server.use(router);
+server.listen(3000, () => {
+	console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
