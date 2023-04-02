f386430c490c9fb786404a3823f4e102fda29938
Kamil Stecyk <kamils987@interia.eu>
2023-04-02 15:13:12 +0200

    add angular front

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 // JSON Server module
+const express = require('express');
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
+
+const app = express();
 
 server.use(middlewares);
 // Add this before server.use(router)
 // server.use(
 // 	// Add custom route here if needed
 // );
+
+app.use(express.static(__dirname + '/dist'));
 
 jsonServer.rewriter({"/api/*": "/$1",})
 server.use(router);
